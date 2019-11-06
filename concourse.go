d6037c85a84c3bd16e1c2b7c1df22d9df4762f41
Rob Whitby <rob.whitby@springer.com>
2019-11-06 15:46:01 +0000

    response from concourse check resource is now 201

--- concourse.go
+++ concourse.go
@@ -82,7 +82,7 @@ func (c Concourse) CheckResource(resource Resource) error {
 	}
 	defer res.Body.Close()
 
-	if res.StatusCode != 200 {
+	if res.StatusCode != 201 {
 		return fmt.Errorf("error calling concourse api: %v %s", res.StatusCode, res.Status)
 	}
 
